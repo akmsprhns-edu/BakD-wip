bdc8578c2bb91fd613dea17f3618577f84090af1
Akims <Akims3105@gmail.com>
2021-04-02 14:49:43 +0300

    commit

--- TestOnnxInferenceSpeed/TestOnnxInferenceSpeed/Program.cs
+++ TestOnnxInferenceSpeed/TestOnnxInferenceSpeed/Program.cs
@@ -1,26 +1,27 @@
 ﻿using Microsoft.ML;
+using Microsoft.ML.OnnxRuntime;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace TestOnnxInferenceSpeed
 {
     class Program
     {
+        private static readonly string ModelPath = "./model.1.onnx";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var mlContext = new MLContext();
+
+
+
+
+
+
             var rand = new Random();
-            var dummyData = mlContext.Data.LoadFromEnumerable(new InputData[0]);
-
-            int? gpu = null;
-            var pipeline = mlContext.Transforms.ApplyOnnxModel("./simple.onnx");
-            var transformer = pipeline.Fit(dummyData);
-            var predictionEngine = mlContext.Model.CreatePredictionEngine<InputData, Prediction>(transformer);
-
             Console.WriteLine("Generationg random data");
             var inputDataList = new List<InputData>();
-            for(int i = 0; i < 100; i++)
+            for(int i = 0; i < 10000; i++)
             {
                 var inputData = new InputData()
                 {
@@ -33,16 +34,42 @@ namespace TestOnnxInferenceSpeed
                 }
                 inputDataList.Add(inputData);
             }
+            var threads = new List<Thread>();
 
+            var mlContext = new MLContext();
+            var dummyData = mlContext.Data.LoadFromEnumerable(new InputData[0]);
+
+
+            Console.WriteLine("Prepearing threads");
+            for (int t = 0; t < 16; t++)
+            {
+                threads.Add(new Thread(() =>
+                {
+                    
+                    int? gpu = null;
+                    var pipeline = mlContext.Transforms.ApplyOnnxModel(ModelPath, gpuDeviceId: gpu);
+                    var transformer = pipeline.Fit(dummyData);
+                    var predictionEngine = mlContext.Model.CreatePredictionEngine<InputData, Prediction>(transformer);
+                    //var inferenceSession = new InferenceSession(ModelPath, SessionOptions.MakeSessionOptionWithCudaProvider(0));
+                    //var container = new List<NamedOnnxValue>();
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        var data = mlContext.Data.LoadFromEnumerable(inputDataList);
+                        foreach (var item in inputDataList)
+                        {
+                            _ = predictionEngine.Predict(item).Evaluation;
+                        }
+                    }
+                }));
+            }
             Console.WriteLine("Starting inference");
             var start = DateTime.Now;
-            for (int i = 0; i < 1000; i++)
-            {
-                foreach (var item in inputDataList)
-                {
-                    _ = predictionEngine.Predict(item).Evaluation;
-                }
-            }
+            foreach (var thread in threads)
+                thread.Start();
+
+            foreach (var thread in threads)
+                thread.Join();
 
             var duration = DateTime.Now - start;
             Console.WriteLine($"###############DONE in {duration.TotalSeconds} seconds################");
